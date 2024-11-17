1ba32a70a5ffe444a87b326674f17b578615b0b5
rabi-siddique <rabisiddique27@gmail.com>
2024-11-17 17:03:30 +0500

    fix: create a vite plugin to remove crossorigin attribute

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite';
 
+function removeCrossorigin() {
+  return {
+    name: 'remove-crossorigin',
+    enforce: 'post',
+    transformIndexHtml(html) {
+      return html.replace(/\s+crossorigin/g, '');
+    },
+  };
+}
+
 export default defineConfig({
   base: './',
+  plugins: [removeCrossorigin()],
 });
